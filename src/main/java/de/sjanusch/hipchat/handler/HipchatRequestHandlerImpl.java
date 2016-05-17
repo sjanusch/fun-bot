03de50c5fe7b36cb4fb9674373af33f210735923
Sandro Janusch <sjanusch@seibert-media.net>
2016-05-17 11:54:03 +0200

    Connection Configurierbar gemacht

--- src/main/java/de/sjanusch/hipchat/handler/HipchatRequestHandlerImpl.java
+++ src/main/java/de/sjanusch/hipchat/handler/HipchatRequestHandlerImpl.java
@@ -1,10 +1,13 @@
 package de.sjanusch.hipchat.handler;
 
 import com.google.inject.Inject;
+import de.sjanusch.configuration.BotConfiguration;
 import de.sjanusch.model.Room;
 import de.sjanusch.networking.Connection;
 import org.jivesoftware.smack.XMPPException;
 import org.jivesoftware.smackx.muc.MultiUserChat;
+
+import java.io.IOException;
 
 /**
  * Created by Sandro Janusch
@@ -13,17 +16,18 @@ import org.jivesoftware.smackx.muc.MultiUserChat;
  */
 public class HipchatRequestHandlerImpl implements HipchatRequestHandler {
 
-    private final String apiKey = "0AvDGI72wHaRkWHUGIwcHKDiQRLav0RQ080Lnvt6";
+    private final BotConfiguration botConfiguration;
 
     final private Connection connection;
 
     @Inject
-    public HipchatRequestHandlerImpl(final Connection connection) {
+    public HipchatRequestHandlerImpl(final BotConfiguration botConfiguration, final Connection connection) {
+        this.botConfiguration = botConfiguration;
         this.connection = connection;
     }
 
-    public void sendMessageToRoom(final String name, final String message) {
-        final Room room = connection.findRoom(name, apiKey);
+    public void sendMessageToRoom(final String name, final String message) throws IOException {
+        final Room room = connection.findRoom(name, botConfiguration.getBotChatApikey());
         if (room != null) {
             sendMessage(message);
         }
