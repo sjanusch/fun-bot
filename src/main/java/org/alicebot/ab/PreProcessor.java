eab3af5f21873ee1fa922fcfeb9b58d6efe74264
Sandro Janusch <sjanusch@seibert-media.net>
2016-06-22 11:58:55 +0200

    URL Behandlung

--- src/main/java/org/alicebot/ab/PreProcessor.java
+++ src/main/java/org/alicebot/ab/PreProcessor.java
@@ -19,6 +19,9 @@ package org.alicebot.ab;
         Boston, MA  02110-1301, USA.
 */
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -33,245 +36,285 @@ import java.util.regex.Pattern;
  * AIML Preprocessor and substitutions
  */
 public class PreProcessor {
-	static private boolean DEBUG = false;
 
-    public int normalCount = 0;
-    public int denormalCount = 0;
-    public int personCount = 0;
-    public int person2Count = 0;
-    public int genderCount = 0;
-    public String[] normalSubs = new String[MagicNumbers.max_substitutions];
-    public Pattern[] normalPatterns = new Pattern[MagicNumbers.max_substitutions];
-    public String[] denormalSubs = new String[MagicNumbers.max_substitutions];
-    public Pattern[] denormalPatterns = new Pattern[MagicNumbers.max_substitutions];
-    public String[] personSubs = new String[MagicNumbers.max_substitutions];
-    public Pattern[] personPatterns = new Pattern[MagicNumbers.max_substitutions];
-    public String[] person2Subs = new String[MagicNumbers.max_substitutions];
-    public Pattern[] person2Patterns = new Pattern[MagicNumbers.max_substitutions];
-    public String[] genderSubs = new String[MagicNumbers.max_substitutions];
-    public Pattern[] genderPatterns = new Pattern[MagicNumbers.max_substitutions];
+  static private boolean DEBUG = false;
 
-    /**
-     * Constructor given bot
-     *
-     * @param bot       AIML bot
-     */
-    public PreProcessor (Bot bot) {
+  private static final Logger logger = LoggerFactory.getLogger(PreProcessor.class);
 
-        normalCount = readSubstitutions(bot.getConfig_path() + "/normal.txt", normalPatterns, normalSubs);
-        denormalCount = readSubstitutions(bot.getConfig_path() + "/denormal.txt", denormalPatterns, denormalSubs);
-        personCount = readSubstitutions(bot.getConfig_path() + "/person.txt", personPatterns, personSubs);
-        person2Count = readSubstitutions(bot.getConfig_path() + "/person2.txt", person2Patterns, person2Subs);
-        genderCount = readSubstitutions(bot.getConfig_path() + "/gender.txt", genderPatterns, genderSubs);
-        if (MagicBooleans.trace_mode) System.out.println("Preprocessor: "+normalCount+" norms "+personCount+" persons "+person2Count+" person2 ");
+  public int normalCount = 0;
+
+  public int denormalCount = 0;
+
+  public int personCount = 0;
+
+  public int person2Count = 0;
+
+  public int genderCount = 0;
+
+  public String[] normalSubs = new String[MagicNumbers.max_substitutions];
+
+  public Pattern[] normalPatterns = new Pattern[MagicNumbers.max_substitutions];
+
+  public String[] denormalSubs = new String[MagicNumbers.max_substitutions];
+
+  public Pattern[] denormalPatterns = new Pattern[MagicNumbers.max_substitutions];
+
+  public String[] personSubs = new String[MagicNumbers.max_substitutions];
+
+  public Pattern[] personPatterns = new Pattern[MagicNumbers.max_substitutions];
+
+  public String[] person2Subs = new String[MagicNumbers.max_substitutions];
+
+  public Pattern[] person2Patterns = new Pattern[MagicNumbers.max_substitutions];
+
+  public String[] genderSubs = new String[MagicNumbers.max_substitutions];
+
+  public Pattern[] genderPatterns = new Pattern[MagicNumbers.max_substitutions];
+
+  /**
+   * Constructor given bot
+   *
+   * @param bot AIML bot
+   */
+  public PreProcessor(Bot bot) {
+
+    normalCount = readSubstitutions(bot.getConfig_path() + "/normal.txt", normalPatterns, normalSubs);
+    denormalCount = readSubstitutions(bot.getConfig_path() + "/denormal.txt", denormalPatterns, denormalSubs);
+    personCount = readSubstitutions(bot.getConfig_path() + "/person.txt", personPatterns, personSubs);
+    person2Count = readSubstitutions(bot.getConfig_path() + "/person2.txt", person2Patterns, person2Subs);
+    genderCount = readSubstitutions(bot.getConfig_path() + "/gender.txt", genderPatterns, genderSubs);
+    if (MagicBooleans.trace_mode) System.out.println("Preprocessor: " + normalCount + " norms " + personCount + " persons " + person2Count + " person2 ");
+  }
+
+  /**
+   * apply normalization substitutions to a request
+   *
+   * @param request client input
+   * @return normalized client input
+   */
+  public String normalize(String request) {
+    logger.debug("PreProcessor.normalize(request: " + request + ")");
+    String newRequest = removeUrl(request);
+    String result = substitute(newRequest, normalPatterns, normalSubs, normalCount);
+    result = result.replaceAll("(\r\n|\n\r|\r|\n)", " ");
+    logger.debug("PreProcessor.normalize() returning: " + result);
+    return result;
+  }
+
+  private static String removeUrl(String commentstr) {
+    String urlPattern = "((https?|ftp|gopher|telnet|file|Unsure|http|www?):((//)|(\\\\))+[\\w\\d:#@%/;$()~_?\\+-=\\\\\\.&]*)";
+    Pattern p = Pattern.compile(urlPattern, Pattern.CASE_INSENSITIVE);
+    Matcher m = p.matcher(commentstr);
+    int i = 0;
+    while (m.find()) {
+      commentstr = commentstr.replaceAll(m.group(i), "").trim();
+      if (commentstr.contains("www.")) {
+        commentstr = "";
+      }
+      i++;
     }
+    return commentstr;
+  }
 
-    /**
-     * apply normalization substitutions to a request
-     *
-     * @param request  client input
-     * @return         normalized client input
-     */
-    public String normalize (String request) {
-		if (DEBUG) System.out.println("PreProcessor.normalize(request: " + request + ")");
-		String result = substitute(request,  normalPatterns, normalSubs, normalCount);
-        result = result.replaceAll("(\r\n|\n\r|\r|\n)", " ");
-		if (DEBUG) System.out.println("PreProcessor.normalize() returning: " + result);
-        return result;
-    }
-    /**
-     * apply denormalization substitutions to a request
-     *
-     * @param request  client input
-     * @return         normalized client input
-     */
-    public String denormalize (String request) {
-        return substitute(request,  denormalPatterns, denormalSubs, denormalCount);
-    }
-    /**
-     * personal pronoun substitution for {@code <person></person>} tag
-     * @param input  sentence
-     * @return       sentence with pronouns swapped
-     */
-    public String person (String input) {
-        return substitute(input, personPatterns, personSubs, personCount);
+  /**
+   * apply denormalization substitutions to a request
+   *
+   * @param request client input
+   * @return normalized client input
+   */
+  public String denormalize(String request) {
+    return substitute(request, denormalPatterns, denormalSubs, denormalCount);
+  }
 
-    }
-    /**
-     * personal pronoun substitution for {@code <person2></person2>} tag
-     * @param input  sentence
-     * @return       sentence with pronouns swapped
-     */
-    public String person2 (String input) {
-        return substitute(input, person2Patterns, person2Subs, person2Count);
+  /**
+   * personal pronoun substitution for {@code <person></person>} tag
+   *
+   * @param input sentence
+   * @return sentence with pronouns swapped
+   */
+  public String person(String input) {
+    return substitute(input, personPatterns, personSubs, personCount);
 
-    }
-    /**
-     * personal pronoun substitution for {@code <gender>} tag
-     * @param input  sentence
-     * @return       sentence with pronouns swapped
-     */
-    public String gender (String input) {
-        return substitute(input, genderPatterns, genderSubs, genderCount);
+  }
 
-    }
+  /**
+   * personal pronoun substitution for {@code <person2></person2>} tag
+   *
+   * @param input sentence
+   * @return sentence with pronouns swapped
+   */
+  public String person2(String input) {
+    return substitute(input, person2Patterns, person2Subs, person2Count);
 
-    /**
-     * Apply a sequence of subsitutions to an input string
-     *
-     * @param request      input request
-     * @param patterns     array of patterns to match
-     * @param subs         array of substitution values
-     * @param count        number of patterns and substitutions
-     * @return             result of applying substitutions to input
-     */
-    String substitute(String request, Pattern[] patterns, String[] subs, int count) {
-        String result = " "+request+" ";
-        int index=0;
-        try {
-        for (int i = 0; i < count; i++) {
-            index = i;
-            String replacement =  subs[i];
-            Pattern p = patterns[i];
-            Matcher m = p.matcher(result);
-            //System.out.println(i+" "+patterns[i].pattern()+"-->"+subs[i]);
-            if (m.find()) {
-                //System.out.println(i+" "+patterns[i].pattern()+"-->"+subs[i]);
-                //System.out.println(m.group());
-                result = m.replaceAll(replacement);
+  }
+
+  /**
+   * personal pronoun substitution for {@code <gender>} tag
+   *
+   * @param input sentence
+   * @return sentence with pronouns swapped
+   */
+  public String gender(String input) {
+    return substitute(input, genderPatterns, genderSubs, genderCount);
+
+  }
+
+  /**
+   * Apply a sequence of subsitutions to an input string
+   *
+   * @param request  input request
+   * @param patterns array of patterns to match
+   * @param subs     array of substitution values
+   * @param count    number of patterns and substitutions
+   * @return result of applying substitutions to input
+   */
+  String substitute(String request, Pattern[] patterns, String[] subs, int count) {
+    String result = " " + request + " ";
+    int index = 0;
+    try {
+      for (int i = 0; i < count; i++) {
+        index = i;
+        String replacement = subs[i];
+        Pattern p = patterns[i];
+        Matcher m = p.matcher(result);
+        //System.out.println(i+" "+patterns[i].pattern()+"-->"+subs[i]);
+        if (m.find()) {
+          //System.out.println(i+" "+patterns[i].pattern()+"-->"+subs[i]);
+          //System.out.println(m.group());
+          result = m.replaceAll(replacement);
+        }
+
+        //System.out.println(result);
+      }
+      while (result.contains("  ")) result = result.replace("  ", " ");
+      result = result.trim();
+      //System.out.println("Normalized: "+result);
+    } catch (Exception ex) {
+      ex.printStackTrace();
+      System.out.println("Request " + request + " Result " + result + " at " + index + " " + patterns[index] + " " + subs[index]);
+    }
+    return result.trim();
+  }
+
+  /**
+   * read substitutions from input stream
+   *
+   * @param in       input stream
+   * @param patterns array of patterns
+   * @param subs     array of substitution values
+   * @return number of patterns substitutions read
+   */
+  public int readSubstitutionsFromInputStream(InputStream in, Pattern[] patterns, String[] subs) {
+    BufferedReader br = new BufferedReader(new InputStreamReader(in));
+    String strLine;
+    //Read File Line By Line
+    int subCount = 0;
+    try {
+      while ((strLine = br.readLine()) != null) {
+        //System.out.println(strLine);
+        strLine = strLine.trim();
+        if (!strLine.startsWith(MagicStrings.text_comment_mark)) {
+          Pattern pattern = Pattern.compile("\"(.*?)\",\"(.*?)\"", Pattern.DOTALL);
+          Matcher matcher = pattern.matcher(strLine);
+          if (matcher.find() && subCount < MagicNumbers.max_substitutions) {
+            subs[subCount] = matcher.group(2);
+            String quotedPattern = Pattern.quote(matcher.group(1));
+            //System.out.println("quoted pattern="+quotedPattern);
+            patterns[subCount] = Pattern.compile(quotedPattern, Pattern.CASE_INSENSITIVE);
+            subCount++;
+          }
+        }
+
+      }
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+    return subCount;
+  }
+
+  /**
+   * read substitutions from a file
+   *
+   * @param filename name of substitution file
+   * @param patterns array of patterns
+   * @param subs     array of substitution values
+   * @return number of patterns and substitutions read
+   */
+  int readSubstitutions(String filename, Pattern[] patterns, String[] subs) {
+    int subCount = 0;
+    try {
+
+      // Open the file that is the first
+      // command line parameter
+      File file = new File(filename);
+      if (file.exists()) {
+        FileInputStream fstream = new FileInputStream(filename);
+        // Get the object of DataInputStream
+        subCount = readSubstitutionsFromInputStream(fstream, patterns, subs);
+        //Close the input stream
+        fstream.close();
+      }
+    } catch (Exception e) {//Catch exception if any
+      System.err.println("Error: " + e.getMessage());
+    }
+    return (subCount);
+  }
+
+  /**
+   * Split an input into an array of sentences based on sentence-splitting characters.
+   *
+   * @param line input text
+   * @return array of sentences
+   */
+  public String[] sentenceSplit(String line) {
+    line = line.replace("。", ".");
+    line = line.replace("？", "?");
+    line = line.replace("！", "!");
+    //System.out.println("Sentence split "+line);
+    String result[] = line.split("[\\.!\\?]");
+    for (int i = 0; i < result.length; i++) result[i] = result[i].trim();
+    return result;
+  }
+
+  /**
+   * normalize a file consisting of sentences, one sentence per line.
+   *
+   * @param infile  input file
+   * @param outfile output file to write results
+   */
+  public void normalizeFile(String infile, String outfile) {
+    try {
+      BufferedWriter bw = null;
+      FileInputStream fstream = new FileInputStream(infile);
+      BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+      bw = new BufferedWriter(new FileWriter(outfile));
+      String strLine;
+      //Read File Line By Line
+      while ((strLine = br.readLine()) != null) {
+        strLine = strLine.trim();
+        if (strLine.length() > 0) {
+          String norm = normalize(strLine).toUpperCase();
+          String sentences[] = sentenceSplit(norm);
+          {
+            if (sentences.length > 1) {
+              for (String s : sentences)
+                System.out.println(norm + "-->" + s);
             }
-
-            //System.out.println(result);
-        }
-        while (result.contains("  ")) result = result.replace("  "," ");
-        result = result.trim();
-        //System.out.println("Normalized: "+result);
-        } catch (Exception ex)    {
-            ex.printStackTrace();
-            System.out.println("Request "+request+" Result "+result+" at "+index+" "+patterns[index]+" "+subs[index]);
-        }
-        return result.trim();
-    }
-
-    /**
-     * read substitutions from input stream
-     *
-     * @param in        input stream
-     * @param patterns  array of patterns
-     * @param subs      array of substitution values
-     * @return          number of patterns substitutions read
-     */
-    public int readSubstitutionsFromInputStream(InputStream in, Pattern[] patterns, String[] subs) {
-        BufferedReader br = new BufferedReader(new InputStreamReader(in));
-        String strLine;
-        //Read File Line By Line
-        int subCount = 0;
-        try {
-            while ((strLine = br.readLine()) != null)   {
-                //System.out.println(strLine);
-                strLine = strLine.trim();
-                if (!strLine.startsWith(MagicStrings.text_comment_mark)) {
-                    Pattern pattern = Pattern.compile("\"(.*?)\",\"(.*?)\"", Pattern.DOTALL);
-                    Matcher matcher = pattern.matcher(strLine);
-                    if (matcher.find() && subCount < MagicNumbers.max_substitutions) {
-                        subs[subCount] = matcher.group(2);
-                        String quotedPattern = Pattern.quote(matcher.group(1));
-                        //System.out.println("quoted pattern="+quotedPattern);
-                        patterns[subCount] = Pattern.compile(quotedPattern, Pattern.CASE_INSENSITIVE);
-                        subCount++;
-                    }
-                }
-
+            for (String sentence : sentences) {
+              sentence = sentence.trim();
+              if (sentence.length() > 0) {
+                //System.out.println("'"+strLine+"'-->'"+norm+"'");
+                bw.write(sentence);
+                bw.newLine();
+              }
             }
-        } catch (Exception ex) {
-            ex.printStackTrace();
+          }
         }
-        return subCount;
+      }
+      bw.close();
+      br.close();
+    } catch (Exception ex) {
+      ex.printStackTrace();
     }
-
-    /**
-     * read substitutions from a file
-     *
-     * @param filename  name of substitution file
-     * @param patterns  array of patterns
-     * @param subs      array of substitution values
-     * @return          number of patterns and substitutions read
-     */
-    int readSubstitutions(String filename, Pattern[] patterns, String[] subs) {
-        int subCount = 0;
-        try{
-
-            // Open the file that is the first
-            // command line parameter
-            File file = new File(filename);
-            if (file.exists()) {
-                FileInputStream fstream = new FileInputStream(filename);
-                // Get the object of DataInputStream
-                subCount = readSubstitutionsFromInputStream(fstream, patterns, subs);
-                //Close the input stream
-                fstream.close();
-            }
-        }catch (Exception e){//Catch exception if any
-            System.err.println("Error: " + e.getMessage());
-        }
-        return (subCount);
-    }
-
-    /**
-     * Split an input into an array of sentences based on sentence-splitting characters.
-     *
-     * @param line   input text
-     * @return       array of sentences
-     */
-    public String[] sentenceSplit(String line) {
-        line = line.replace("。",".");
-        line = line.replace("？","?");
-        line = line.replace("！","!");
-        //System.out.println("Sentence split "+line);
-        String result[] = line.split("[\\.!\\?]");
-        for (int i = 0; i < result.length; i++) result[i] = result[i].trim();
-        return result;
-    }
-
-    /**
-     * normalize a file consisting of sentences, one sentence per line.
-     *
-     * @param infile           input file
-     * @param outfile          output file to write results
-     */
-    public void normalizeFile (String infile, String outfile) {
-        try{
-            BufferedWriter bw = null;
-            FileInputStream fstream = new FileInputStream(infile);
-            BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
-            bw = new BufferedWriter(new FileWriter(outfile)) ;
-            String strLine;
-            //Read File Line By Line
-            while ((strLine = br.readLine()) != null)   {
-                strLine = strLine.trim();
-                if (strLine.length() > 0) {
-                    String norm = normalize(strLine).toUpperCase();
-                    String sentences[] = sentenceSplit(norm); {
-                        if (sentences.length > 1) {
-                            for (String s : sentences)
-                                System.out.println(norm+"-->"+s);
-                        }
-                        for (String sentence : sentences) {
-                            sentence = sentence.trim();
-                            if (sentence.length() > 0) {
-                                //System.out.println("'"+strLine+"'-->'"+norm+"'");
-                                bw.write(sentence);
-                                bw.newLine();
-                            }
-                        }
-                    }
-                }
-            }
-            bw.close();
-            br.close();
-        }
-        catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
+  }
 }
