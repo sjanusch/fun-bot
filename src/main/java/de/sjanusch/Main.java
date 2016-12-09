b9ca940b72e047b468c3a417f5f44f873709f8e1
Sandro Janusch <sjanusch@seibert-media.net>
2016-12-09 12:38:10 +0100

    NSQ Client

--- src/main/java/de/sjanusch/Main.java
+++ src/main/java/de/sjanusch/Main.java
@@ -4,10 +4,11 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import de.sjanusch.bot.Bot;
 import de.sjanusch.guice.GuiceModule;
-import de.sjanusch.nsq.NSQProducer;
 import de.sjanusch.runner.BotRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.walkmod.nsq.NSQProducer;
+import org.walkmod.nsq.exceptions.NSQException;
 
 public class Main {
 
@@ -19,11 +20,20 @@ public class Main {
         final BotRunner botRunner = injector.getInstance(BotRunner.class);
         final Thread t = botRunner.runBotDesync(bot);
 
-        NSQProducer nsqProducer = new NSQProducer("Test");
-        try {
-            nsqProducer.put("Hallo");
-        } catch (Exception e) {
-            logger.error("NSQProducer: " + e.getMessage());
+        NSQProducer producer = new NSQProducer("http://127.0.0.1:4151", "testTopic");
+
+        for(int i=0; i<100; i++) {
+            try {
+                String message = "{\"foo\":\"bar\"}";
+                System.out.println("Sending: " + message);
+                producer.put(message);
+                Thread.sleep(1000);
+            } catch (NSQException e) {
+                logger.error("NSQException " + e.getMessage());
+                System.exit(1);
+            } catch (InterruptedException e) {
+                logger.error("InterruptedException " + e.getMessage());
+            }
         }
 
         t.start();
