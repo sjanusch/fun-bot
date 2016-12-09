1f06d36d172b3772bac2eb4707011d6c60b0cb81
Sandro Janusch <sjanusch@seibert-media.net>
2016-12-09 12:50:40 +0100

    NSQ Client

--- src/main/java/de/sjanusch/Main.java
+++ src/main/java/de/sjanusch/Main.java
@@ -8,35 +8,39 @@ import de.sjanusch.runner.BotRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.walkmod.nsq.NSQProducer;
+import org.walkmod.nsq.example.PrintReader;
 import org.walkmod.nsq.exceptions.NSQException;
 
 public class Main {
 
-    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+  private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
-    public static void main(String[] args) {
-        final Injector injector = Guice.createInjector(new GuiceModule());
-        final Bot bot = injector.getInstance(Bot.class);
-        final BotRunner botRunner = injector.getInstance(BotRunner.class);
-        final Thread t = botRunner.runBotDesync(bot);
+  public static void main(String[] args) {
+    final Injector injector = Guice.createInjector(new GuiceModule());
+    final Bot bot = injector.getInstance(Bot.class);
+    final BotRunner botRunner = injector.getInstance(BotRunner.class);
+    final Thread t = botRunner.runBotDesync(bot);
+    PrintReader.main(args);
 
-        NSQProducer producer = new NSQProducer("http://localhost:4150", "testTopic");
 
-        for(int i=0; i<100; i++) {
-            try {
-                String message = "{\"foo\":\"bar\"}";
-                System.out.println("Sending: " + message);
-                producer.put(message);
-                Thread.sleep(1000);
-            } catch (NSQException e) {
-                logger.error("NSQException " + e.getMessage());
-                System.exit(1);
-            } catch (InterruptedException e) {
-                logger.error("InterruptedException " + e.getMessage());
-            }
-        }
+    NSQProducer producer = new NSQProducer("http://localhost:4150", "testTopic");
 
-        t.start();
+    for (int i = 0; i < 100; i++) {
+      try {
+        String message = "{\"foo\":\"bar\"}";
+        System.out.println("Sending: " + message);
+        producer.put(message);
+        Thread.sleep(1000);
+      } catch (NSQException e) {
+        logger.error("NSQException " + e.getMessage());
+        System.exit(1);
+      } catch (InterruptedException e) {
+        logger.error("InterruptedException " + e.getMessage());
+      }
     }
+
+
+    t.start();
+  }
 
 }
